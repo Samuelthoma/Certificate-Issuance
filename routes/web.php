9203783adc159d94c8f18632e0a11bb85d4183e9
Samuel Thomas <samuel.thomas@student.president.ac.id>
2025-03-18 19:49:22 +0700

    fix: resolve merge conflict

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,11 @@ Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('regi
 Route::get('/welcome', function () {
     return view('pages.home');
 });
+
+Route::post('/send-otp', [OtpController::class, 'sendOtp'])->name('send.otp');
+Route::get('/otp', [OtpController::class, 'showOtpForm'])->name('register.otp.form')->Middleware('session:otp');
+Route::post('/verify-otp', [OtpController::class, 'verifyOtp']);
+
+Route::get('/welcome', function () {
+    return view('pages.home');
+});
